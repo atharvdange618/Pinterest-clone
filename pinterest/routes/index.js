a58676fb7a375849265476837cb71f534c96dc74
atharvdange618 <atharvdange618@gmail.com>
2023-11-22 13:56:09 +0530

    register route complete

--- pinterest/routes/index.js
+++ pinterest/routes/index.js
@@ -2,40 +2,25 @@ var express = require('express');
 var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./posts");
+const passport = require('passport');
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/allposts', async function (req, res, next) {
-    let user = await userModel
-        .findOne({ _id: "655cf5c58a1e8e1c8193af51" })
-        .populate("posts");
-    res.send(user);
-});
+router.post("/register", function (req, res) {
+    const { username, email, fullname } = req.body;
+    const userData = new userModel({ username, email, fullname });
 
-router.get('/createuser', async function (req, res, next) {
-    const createdUser = await userModel.create({
-        username: "mrunal",
-        password: "Deadpool",
-        posts: [],
-        email: "mrunaldange618@gmail.com",
-        fullName: "mrunal Vijayrao Dange"
-    });
-
-    res.send(createdUser);
-});
-
-router.get('/createpost', async function (req, res, next) {
-    const createdPost = await postModel.create({
-        postText: "Ram Ram bhai Saryane !",
-        user: "655cf5c58a1e8e1c8193af51"
-    });
-    let user = await userModel.findOne({ _id: "655cf5c58a1e8e1c8193af51" });
-    user.posts.push(createdPost._id);
-    await user.save();
-    res.send("Done");
+    userModel.register(userData, req.body.password)
+        .then(function () {
+            passport.authenticate("local"), (req, res, function () {
+                res.redirect("/profile");
+            })
+        })
 });
 
 module.exports = router;
