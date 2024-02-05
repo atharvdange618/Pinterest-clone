3a9423adf3a7fef69762922c747f14a5ba349c42
atharvdange618 <atharvdange618@gmail.com>
2024-02-06 03:33:24 +0530

    added functionality to create and display posts and also developed feed page

--- pinterest/routes/index.js
+++ pinterest/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require("./users");
+const postModel = require("./posts");
 const passport = require('passport');
-
+const upload = require('./multer');
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 
@@ -12,15 +13,41 @@ router.get('/', function (req, res, next) {
 });
 
 router.get("/login", function (req, res) {
-    res.render('login');
+    res.render('login', { error: req.flash("error") });
 });
 
-router.get("/feed", function (req, res) {
-    res.render('feed');
+router.get("/feed", async function (req, res) {
+    const user = await userModel.findOne({
+        username: req.session.passport.user
+    });
+    res.render('feed', { user });
 });
 
-router.get("/profile", isLoggedIn, function (req, res, next) {
-    res.render("profile");
+router.post("/upload", isLoggedIn, upload.single('file'), async function (req, res) {
+    if (!req.file) {
+        return res.status(404).send("no files were given");
+    }
+    //save the uploaded file as a post and give it's post id to the user and user id to the post
+    const user = await userModel.findOne({
+        username: req.session.passport.user
+    });
+    const post = await postModel.create({
+        image: req.file.filename,
+        imageText: req.body.filecaption,
+        userid: user._id
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
+
+router.get("/profile", isLoggedIn, async function (req, res, next) {
+    const user = await userModel.findOne({
+        username: req.session.passport.user
+    }).populate("posts");
+    // console.log(user);
+    res.render("profile", { user });
 });
 
 router.post("/register", function (req, res) {
